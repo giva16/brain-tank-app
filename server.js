86e0e525cfb86f32718a35efc761ee2a9937d0b6
giva16 <ltpie1@hotmail.com>
2024-03-07 15:21:06 +0100

    udpated dependencies

--- server.js
+++ server.js
@@ -15,11 +15,6 @@ app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the Project Ideas API' });
 });
 
-// initialize server (server listening for HTTP requests at PORT)
-app.listen(PORT, (req, res) => {
-  console.log(`Server listening at port ${PORT}`);
-});
-
 // connect to our database
 const dbConnect = require('./config/db');
 dbConnect();
@@ -27,3 +22,8 @@ dbConnect();
 // Link server to projects api router
 const projectsRouter = require('./routes/projects');
 app.use('/api/projects', projectsRouter);
+
+// initialize server (server listening for HTTP requests at PORT)
+app.listen(PORT, (req, res) => {
+  console.log(`Server listening at port ${PORT}`);
+});
