7b9092036985e8227989326e86940eb929bd837d
giva16 <ltpie1@hotmail.com>
2024-03-07 22:25:32 +0100

    added font-awesome to front-end

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,2 @@
 // Entry point for our source codes
-import message from "./message.js";
-import './css/style.css'
-
-console.log(message);
-
-const hello = () => console.log()
+import './css/style.css';
